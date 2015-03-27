e17723567398f255dd64de0f34d03ab8432e7026
Vladimir <vmbx.new@gmail.com>
2015-03-27 16:17:10 -0400

    Client, Pagination

---
+++ NETAPI/Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialMyCalls
+{
+    public class Client
+    {
+        public Client(string apiKey) {
+            ApiKey = apiKey;  
+        }
+
+        public string ApiKey {
+            get {
+                return IntApiKey;
+            }
+            set {
+                IntApiKey = value;
+                ApiUrl = string.Format(API_URL, IntApiKey);
+            }
+        }
+
+        public string ApiUrl { get; private set; }
+
+        private const string API_URL = @"https://{0}@api.dialmycalls.com/2.0/";
+
+        private string IntApiKey;
+
+    }
+}
---
+++ NETAPI/Pagination.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialMyCalls
+{
+    public class Pagination
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+    }
+}
