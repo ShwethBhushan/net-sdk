272c323c9cbc55e00e6ab7c7b406ee062a4fb99e
Vladimir <vmbx.new@gmail.com>
2015-04-02 18:36:04 -0400

    Call.cs

---
+++ DialMyCalls-NETAPI/TEST/Call.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DialMyCalls;
+
+namespace TEST
+{
+    public static class Call
+    {
+        public static bool Run() {
+            Console.WriteLine("Generating call..");
+            Client client = new Client(Config.APIKey);
+            var svc = new DialMyCalls.Service.Call(client);
+            svc.Create("Test Call", callerIdId, recordingId, DateTime.Now.AddMonths(1), false, false, false, new List<string>() { "5555555555" }, new Dictionary<string, string>()); 
+
+        }
+    }
+}
